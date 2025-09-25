rusbomber/VictoriaLogs

commit 0bfbe6b18f8f976c6dd4ca6e1980debc4c537be7

lib/logstorage: add canReturnLastNResults() method to pipe interface
This should reduce the probability of incorrect handling of 'last N' results
functionality for newly added pipes.

Add CanReturnLastNResults() tests per every supported pipe.

lib/logstorage/pipe_unroll.go
@@ -41,6 +41,10 @@ func (pu *pipeUnroll) canLiveTail() bool {
 	return true
 }
 
+func (pu *pipeUnroll) canReturnLastNResults() bool {
+	return false
+}
+
 func (pu *pipeUnroll) hasFilterInWithQuery() bool {
 	return pu.iff.hasFilterInWithQuery()
 }

lib/logstorage/parser_test.go
@@ -3120,34 +3120,60 @@ func TestQueryCanReturnLastNResults(t *testing.T) {
 		}
 	}
 
+	// query without pipes
 	f("*", true)
 	f("error", true)
-	f("error | fields foo, _time | filter foo:bar", true)
-	f("error | extract '<foo>bar<baz>'", true)
-	f("* | rm x", true)
-	f("* | stats count() rows", false)
-	f("* | sort by (x)", false)
-	f("* | first 10 by (x)", false)
-	f("* | last 10 by (x)", false)
-	f("* | len(x)", true)
-	f("* | limit 10", false)
-	f("* | offset 10", false)
-	f("* | union (x)", false)
-	f("* | uniq (x)", false)
-	f("* | block_stats", false)
+
+	// verify all the pipes
 	f("* | blocks_count", false)
+	f("* | block_stats", false)
+	f("* | collapse_nums", true)
+	f("* | copy foo bar", true)
+	f("* | decolorize", true)
+	f("* | delete foo, bar", true)
+	f("* | drop_empty_fields", true)
+	f("* | extract '<foo>bar<baz>'", true)
+	f("* | extract_regexp 'foo(?P<bar>baz)'", true)
 	f("* | facets", false)
 	f("* | field_names", false)
+	f("* | fields foo, bar", false)
+	f("* | fields _time, x", true)
 	f("* | field_values x", false)
-	f("* | top 5 by (x)", false)
+	f("* | filter x:y", true)
+	f("* | first 10 (x)", false)
+	f("* | format 'x<y>'", true)
+	f("* | format 'x<y>' as _time", false)
+	f("* | hash(x)", true)
+	f("* | hash(x) as _time", false)
 	f("* | join by (x) (foo)", false)
 	f("* | json_array_len (x)", true)
-	f("* | unpack_fields x", true)
+	f("* | json_array_len (x) as _time", false)
+	f("* | last 10 (x)", false)
+	f("* | len(x)", true)
+	f("* | len(x) as _time", false)
+	f("* | limit 10", false)
+	f("* | math x+y as z", true)
+	f("* | offset 10", false)
+	f("* | pack_json", true)
+	f("* | pack_json as _time", false)
+	f("* | pack_logfmt", true)
+	f("* | pack_logfmt as _time", false)
+	f("* | rename foo bar", true)
+	f("* | replace ('foo', 'bar')", true)
+	f("* | replace_regexp ('foo', 'bar')", true)
+	f("* | sample 10", false)
+	f("* | sort by (x)", false)
+	f("* | stats count()", false)
+	f("* | stream_context before 10 after 20", false)
+	f("* | time_add 1h", true)
+	f("* | top 5 by (x)", false)
+	f("* | union (x)", false)
+	f("* | uniq (x)", false)
 	f("* | unpack_json x", true)
 	f("* | unpack_logfmt x", true)
 	f("* | unpack_syslog x", true)
-	f("* | hash(a)", true)
-	f("* | sample 10", false)
+	f("* | unpack_words x", true)
+	f("* | unroll by (x)", false)
 
 	// There is no _time field
 	f("* | fields foo, bar", false)
@@ -3157,6 +3183,7 @@ func TestQueryCanReturnLastNResults(t *testing.T) {
 	f("* | fields foo, _time", true)
 	f("* | fields *", true)
 	f("* | delete a, b", true)
+	f("* | fields foo, _time | filter foo:bar", true)
 }
 
 func TestQueryCanLiveTail(t *testing.T) {

lib/logstorage/parser.go
@@ -555,32 +555,8 @@ func getOffsetLimitFromPipeSort(ps *pipeSort) (uint64, uint64, bool) {
 // CanReturnLastNResults returns true if time range filter at q can be adjusted for returning the last N results with the biggest _time values.
 func (q *Query) CanReturnLastNResults() bool {
 	for _, p := range q.pipes {
-		switch t := p.(type) {
-		case *pipeBlockStats,
-			*pipeBlocksCount,
-			*pipeFacets,
-			*pipeFieldNames,
-			*pipeFieldValues,
-			*pipeFirst,
-			*pipeJoin,
-			*pipeLast,
-			*pipeLimit,
-			*pipeOffset,
-			*pipeTop,
-			*pipeSample,
-			*pipeSort,
-			*pipeStats,
-			*pipeUnion,
-			*pipeUniq:
+		if !p.canReturnLastNResults() {
 			return false
-		case *pipeFields:
-			if !prefixfilter.MatchFilters(t.fieldFilters, "_time") {
-				return false
-			}
-		case *pipeDelete:
-			if prefixfilter.MatchFilters(t.fieldFilters, "_time") {
-				return false
-			}
 		}
 	}
 	return true

lib/logstorage/pipe.go
@@ -26,6 +26,11 @@ type pipe interface {
 	// See https://docs.victoriametrics.com/victorialogs/querying/#live-tailing
 	canLiveTail() bool
 
+	// canReturnLastNResults must return true if the given pipe can return last N results ordered by _time desc
+	//
+	// The pipe can return last N results if it doesn't modify the _time field.
+	canReturnLastNResults() bool
+
 	// updateNeededFields must update pf with fields it needs and not needs at the input.
 	updateNeededFields(pf *prefixfilter.Filter)
 

lib/logstorage/pipe_block_stats.go
@@ -28,6 +28,10 @@ func (ps *pipeBlockStats) canLiveTail() bool {
 	return false
 }
 
+func (ps *pipeBlockStats) canReturnLastNResults() bool {
+	return false
+}
+
 func (ps *pipeBlockStats) hasFilterInWithQuery() bool {
 	return false
 }

lib/logstorage/pipe_blocks_count.go
@@ -38,6 +38,10 @@ func (pc *pipeBlocksCount) canLiveTail() bool {
 	return false
 }
 
+func (pc *pipeBlocksCount) canReturnLastNResults() bool {
+	return false
+}
+
 func (pc *pipeBlocksCount) updateNeededFields(pf *prefixfilter.Filter) {
 	pf.Reset()
 }

lib/logstorage/pipe_collapse_nums.go
@@ -45,6 +45,10 @@ func (pc *pipeCollapseNums) canLiveTail() bool {
 	return true
 }
 
+func (pc *pipeCollapseNums) canReturnLastNResults() bool {
+	return true
+}
+
 func (pc *pipeCollapseNums) updateNeededFields(pf *prefixfilter.Filter) {
 	updateNeededFieldsForUpdatePipe(pf, pc.field, pc.iff)
 }

lib/logstorage/pipe_copy.go
@@ -41,6 +41,12 @@ func (pc *pipeCopy) canLiveTail() bool {
 	return true
 }
 
+func (pc *pipeCopy) canReturnLastNResults() bool {
+	// TODO: properly verify that the _time field isn't overwritten by non-timestamp.
+
+	return true
+}
+
 func (pc *pipeCopy) updateNeededFields(f *prefixfilter.Filter) {
 	for i := len(pc.srcFieldFilters) - 1; i >= 0; i-- {
 		srcFieldFilter := pc.srcFieldFilters[i]

commit 855beeed18f0a041625511fb773a55786fdba758

lib/logstorage: allow using `unroll` pipe in front of `stats` pipe at `/select/logsql/stats_query` and `/select/logsql/stats_query_range`
The `unroll` pipe doesn't modify the `_time` field, so it is safe to use there.

The bug has been introduced in the commit https://github.com/VictoriaMetrics/VictoriaLogs/commit/0bfbe6b18f8f976c6dd4ca6e1980debc4c537be7

Updates https://github.com/VictoriaMetrics/VictoriaLogs/issues/697

lib/logstorage/pipe_unroll.go
@@ -42,7 +42,7 @@ func (pu *pipeUnroll) canLiveTail() bool {
 }
 
 func (pu *pipeUnroll) canReturnLastNResults() bool {
-	return false
+	return true
 }
 
 func (pu *pipeUnroll) hasFilterInWithQuery() bool {

lib/logstorage/parser_test.go
@@ -3508,7 +3508,7 @@ func TestQueryCanReturnLastNResults(t *testing.T) {
 	f("* | unpack_logfmt x", true)
 	f("* | unpack_syslog x", true)
 	f("* | unpack_words x", true)
-	f("* | unroll by (x)", false)
+	f("* | unroll by (x)", true)
 
 	// There is no _time field
 	f("* | fields foo, bar", false)
@@ -3678,6 +3678,7 @@ func TestQueryGetStatsByFieldsAddGroupingByTime_Success(t *testing.T) {
 	f("* | unpack_logfmt x | count() x", nsecsPerDay, []string{"_time"}, `* | unpack_logfmt from x | stats by (_time:86400000000000) count(*) as x`)
 	f("* | unpack_syslog x | count() x", nsecsPerDay, []string{"_time"}, `* | unpack_syslog from x | stats by (_time:86400000000000) count(*) as x`)
 	f("* | unpack_words x | count() x", nsecsPerDay, []string{"_time"}, `* | unpack_words from x | stats by (_time:86400000000000) count(*) as x`)
+	f("* | unroll by (x) | count() x", nsecsPerDay, []string{"_time"}, `* | unroll by (x) | stats by (_time:86400000000000) count(*) as x`)
 }
 
 func TestQueryGetStatsByFieldsAddGroupingByTime_Failure(t *testing.T) {
@@ -3754,7 +3755,6 @@ func TestQueryGetStatsByFieldsAddGroupingByTime_Failure(t *testing.T) {
 	f("* | top 5 by (x) | count()")
 	f("* | union (x) | count()")
 	f("* | uniq (x) | count()")
-	f("* | unroll by (x) | count()")
 }
 
 func TestQueryGetStatsByFields_Success(t *testing.T) {