rusbomber/VictoriaLogs

commit ef0440ec0cda69e43bc04ba7eeefdd05a587d737

lib/logstorage: merge data blocks more aggressively
Prevsiously small blocks weren't merged with the next block if its' size was bigger or equal to maxUncompressedBlockSize.
This could result into up to 2x bigger number of blocks than needed, since big blocks
with sizes [maxUncompressedBlockSize ... 2*maxUncompressedBlockSize] were left interleaved with small blocks
with sizes smaller than maxUncompressedBlockSize.

Fix this by merging small blocks with the next big block if their total size doesn't exceed 2*maxUncompressedBlockSize.
This should reduce the number of blocks in the output merged part by up to 2x. This should also improve on-disk
compression ratio and query performance a bit because of the lower overhead on block headers.

lib/logstorage/block_stream_merger.go
@@ -11,7 +11,8 @@ import (
 
 // mustMergeBlockStreams merges bsrs to bsw and updates ph accordingly.
 //
-// Finalize() is guaranteed to be called on bsrs and bsw before returning from the func.
+// Finalize() is guaranteed to be called on bsw before returning from the func.
+// MustClose() is guatanteed to be called on bsrs before returning from the func.
 func mustMergeBlockStreams(ph *partHeader, bsw *blockStreamWriter, bsrs []*blockStreamReader, stopCh <-chan struct{}) {
 	bsm := getBlockStreamMerger()
 	bsm.mustInit(bsw, bsrs)
@@ -106,6 +107,24 @@ func (bsm *blockStreamMerger) resetRows() {
 	bsm.uncompressedRowsSizeBytes = 0
 }
 
+func (bsm *blockStreamMerger) assertNoRows() {
+	if bsm.bd.rowsCount > 0 {
+		logger.Panicf("BUG: bsm.bd must be empty; got %d rows", bsm.bd.rowsCount)
+	}
+	if len(bsm.a.b) > 0 {
+		logger.Panicf("BUG: bsm.a must be empty; got %d bytes", len(bsm.a.b))
+	}
+	if len(bsm.rows.timestamps) > 0 {
+		logger.Panicf("BUG: bsm.rows must be empty; got %d rows", len(bsm.rows.timestamps))
+	}
+	if len(bsm.rowsTmp.timestamps) > 0 {
+		logger.Panicf("BUG: bsm.rowsTmp must be empty; got %d rows", len(bsm.rowsTmp.timestamps))
+	}
+	if bsm.uncompressedRowsSizeBytes != 0 {
+		logger.Panicf("BUG: bsm.uncompressedRowsSizeBytes must be 0; got %d", bsm.uncompressedRowsSizeBytes)
+	}
+}
+
 func (bsm *blockStreamMerger) mustInit(bsw *blockStreamWriter, bsrs []*blockStreamReader) {
 	bsm.reset()
 
@@ -128,26 +147,18 @@ func (bsm *blockStreamMerger) mustWriteBlock(bd *blockData) {
 	switch {
 	case !bd.streamID.equal(&bsm.streamID):
 		// The bd contains another streamID.
-		// Write the current log entries under the current streamID, then process the bd.
+		// Write the bsm logs under the current streamID, then process the bd.
 		bsm.mustFlushRows()
 		bsm.streamID = bd.streamID
-		if bd.uncompressedSizeBytes >= maxUncompressedBlockSize {
-			// Fast path - write full bd to the output without extracting log entries from it.
-			bsm.bsw.MustWriteBlockData(bd)
-		} else {
-			// Slow path - copy the bd to the curr bd.
-			bsm.bd.copyFrom(&bsm.a, bd)
-		}
-	case bd.uncompressedSizeBytes >= maxUncompressedBlockSize:
-		// The bd contains the same streamID and it is full,
-		// so it can be written next after the current log entries
-		// without the need to merge the bd with the current log entries.
-		// Write the current log entries and then the bd.
+		bsm.mustWriteBlockData(bd)
+	case bsm.uncompressedRowsSizeBytes+bd.uncompressedSizeBytes >= 2*maxUncompressedBlockSize:
+		// The bd cannot be merged with bsm, since the final block size will be too big.
+		// Write the bsm logs, then process the bd.
 		bsm.mustFlushRows()
-		bsm.bsw.MustWriteBlockData(bd)
+		bsm.mustWriteBlockData(bd)
 	default:
-		// The bd contains the same streamID and it isn't full,
-		// so it must be merged with the current log entries.
+		// The bd contains the same streamID and the summary size of bsm logs and bd doesn't exceed the maximum allowed.
+		// Merge them.
 		bsm.mustMergeRows(bd)
 	}
 }
@@ -196,6 +207,19 @@ func (bsm *blockStreamMerger) ReadersPaths() string {
 	return fmt.Sprintf("[%s]", strings.Join(paths, ","))
 }
 
+// mustWriteBlockData writes bd to bsm.
+func (bsm *blockStreamMerger) mustWriteBlockData(bd *blockData) {
+	bsm.assertNoRows()
+
+	if bd.uncompressedSizeBytes >= maxUncompressedBlockSize {
+		// Fast path - write full bd to the output without extracting log entries from it.
+		bsm.bsw.MustWriteBlockData(bd)
+		return
+	}
+
+	bsm.bd.copyFrom(&bsm.a, bd)
+}
+
 // mustMergeRows merges the current log entries inside bsm with bd log entries.
 func (bsm *blockStreamMerger) mustMergeRows(bd *blockData) {
 	if bsm.bd.rowsCount > 0 {
@@ -223,6 +247,7 @@ func (bsm *blockStreamMerger) mustMergeRows(bd *blockData) {
 
 func (bsm *blockStreamMerger) mustUnmarshalRows(bd *blockData) {
 	rowsLen := len(bsm.rows.timestamps)
+
 	if bsm.sbu == nil {
 		bsm.sbu = getStringsBlockUnmarshaler()
 	}
@@ -232,6 +257,7 @@ func (bsm *blockStreamMerger) mustUnmarshalRows(bd *blockData) {
 	if err := bd.unmarshalRows(&bsm.rows, bsm.sbu, bsm.vd); err != nil {
 		logger.Panicf("FATAL: cannot merge %s: cannot unmarshal log entries from blockData: %s", bsm.ReadersPaths(), err)
 	}
+
 	bsm.uncompressedRowsSizeBytes += uncompressedRowsSizeBytes(bsm.rows.rows[rowsLen:])
 }
 

commit 56432cd88de8bf0c1f510589951d1d7638db5264

lib/logstorage: prevent from re-packing full block if the current block is empty
This saves CPU time on block repacking.

The issue has been introduced in the commit ef0440ec0cda69e43bc04ba7eeefdd05a587d737.

lib/logstorage/block_stream_merger.go
@@ -151,6 +151,9 @@ func (bsm *blockStreamMerger) mustWriteBlock(bd *blockData) {
 		bsm.mustFlushRows()
 		bsm.streamID = bd.streamID
 		bsm.mustWriteBlockData(bd)
+	case bsm.uncompressedRowsSizeBytes == 0 && bd.uncompressedSizeBytes >= maxUncompressedBlockSize:
+		// The bsm is empty and the bd is full. Just write db to the output.
+		bsm.mustWriteBlockData(bd)
 	case bsm.uncompressedRowsSizeBytes+bd.uncompressedSizeBytes >= 2*maxUncompressedBlockSize:
 		// The bd cannot be merged with bsm, since the final block size will be too big.
 		// Write the bsm logs, then process the bd.